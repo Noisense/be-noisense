28b0a323f8a500714a14f8a8b6b78a3bb6492134
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2023-09-21 09:11:19 +0700

    <feat>route: add logging

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 import os
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(filename='app.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 # MySQL configurations
 app.config['MYSQL_USER'] = 'root'
@@ -34,8 +37,10 @@ def add_recording():
             (audio_id, audio_title, audio_timestamp, audio_path, audio_size, audio_duration)
         )
         mysql.connection.commit()
-        return jsonify(status='success', data=audio_id)
+        logging.info(f"Data successfully added with audio_id: {audio_id}")
+        return jsonify(status='success',  message='Data successfully added.', data={"audio_id": audio_id})
     except Exception as e:
+        logging.error(f"Error adding data: {str(e)}")
         return jsonify(status='error', message=str(e))
     
 
